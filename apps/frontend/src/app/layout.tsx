46731fa97c366f0705420c0507871270ba4767f4
subh <harvirbuttar2917@gmail.com>
2024-08-01 19:41:09 -0700

    added oswald font family an will be used as feom classname of '.oswald'

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Oswald } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
+const oswald = Oswald({ subsets: ["latin"], display: "swap", variable: "--font-poppins" })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${oswald.variable}`}>{children}</body>
     </html>
   );
 }
