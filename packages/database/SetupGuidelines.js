424ce59c4682ac4a404efea9a2a72ff4b3ee702e
subh <harvirbuttar2917@gmail.com>
2024-08-03 11:38:14 -0700

    added more instruction in setupGuidelinesTXT file

--- packages/database/SetupGuidelines.js
+++ packages/database/SetupGuidelines.js
@@ -4,22 +4,22 @@ This file tells the instructions which were followed to setup the prisma and to 
 [1] Add a new database package (packages/database/package.json)
     // Before
     {
-    "name": "database", // change
-    "version": "1.0.0", // change
-    "main": "index.js", // remove
-    "license": "MIT" // remove
+        "name": "database", // change
+        "version": "1.0.0", // change
+        "main": "index.js", // remove
+        "license": "MIT" // remove
     }
 
     // After
     {
-    "name": "@repo/db",
-    "version": "0.0.0",
-    "dependencies": {
-        "@prisma/client": "latest" // Replace with latest version
-    },
-    "devDependencies": {
-        "prisma": "latest" // Replace with latest version
-    }
+        "name": "@repo/db",
+        "version": "0.0.0",
+        "dependencies": {
+            "@prisma/client": "latest" // Replace with latest version
+        },
+        "devDependencies": {
+            "prisma": "latest" // Replace with latest version
+        }
     }		
     <-- CMD -!>
 	[1.1] npx prisma init // to create prisma.schema and more 
@@ -27,10 +27,10 @@ This file tells the instructions which were followed to setup the prisma and to 
 [2] Create Scripts (packages/database/package.json)
     <-- following //After -!>
         {
-        "scripts": {
-            "db:generate": "prisma generate",
-            "db:push": "prisma db push --skip-generate"
-        }
+            "scripts": {
+                "db:generate": "prisma generate",
+                "db:push": "prisma db push --skip-generate"
+            }
         }
     <-- Important -!>
     // Now we have to define these scripts in the (turbo.json) at root
@@ -45,5 +45,10 @@ This file tells the instructions which were followed to setup the prisma and to 
                 }	
             }
         }
-    
+    <-- CMD -!>
+    (At root of project) turbo db:push db:generate
+
+    <-- HEADS UP -!>
+    // Use --skip generate flag on db:push
+
 */
